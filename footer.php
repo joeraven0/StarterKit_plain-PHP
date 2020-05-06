1882a5ae77fd8ad26d11e12116e5ceead5732252
Joe Raven <joakim.ringstad.raavi@gmail.com>
2020-05-06 20:36:40 +0200

    Add files via upload

---
+++ footer.php
@@ -0,0 +1,15 @@
+<footer>
+<h1>Footer</h1>
+</footer>
+</body>
+<link rel="stylesheet" type="text/css" href="css/body.css">
+<link rel="stylesheet" type="text/css" href="css/header.css">
+<link rel="stylesheet" type="text/css" href="css/nav.css">
+<link rel="stylesheet" type="text/css" href="css/main.css">
+<link rel="stylesheet" type="text/css" href="css/footer.css">
+<script type="text/javascript" src=" https://code.jquery.com/jquery-1.12.4.min.js"></script>
+<script type="text/javascript" src="http://code.jquery.com/ui/1.10.3/jquery-ui.js"></script>
+<script type="text/javascript" src="js/jquery.js"/></script>
+<script type="text/javascript" src="js/jqueryui.js"/></script>
+<script type="text/javascript" src="js/javascript.js"/></script>
+</html>
